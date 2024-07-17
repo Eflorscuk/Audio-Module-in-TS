b186245db5926125dd44bd70f35b0003124999f9
Evandro Florscuk <evandro.florscuk@helpertecnologia.com.br>
2024-07-17 13:38:25 -0300

    Instalar express, criar server para escutar na porta 3000. Criar arquivo para routes.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-const message: string = 'Hello, world!';
-console.log(message);
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
