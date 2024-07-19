dabc246e72238a7e5adbdc4de62dac03312dd507
Evandro Florscuk <evandro.florscuk@helpertecnologia.com.br>
2024-07-19 15:52:15 -0300

    Inserir a rota de audios para testes

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -8,3 +8,11 @@ describe('Testando a rota /', () => {
     expect(response.text).toBe("Está funcionando");
   });
 });
+
+describe('Testando a rota /play_sound', () => {
+  it('Deve retornar status 200 e a mensagem "Áudio tocando!"', async () => {
+    const response = await request(app).post('/api/play_sound');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Áudio tocando!");
+  });
+});
