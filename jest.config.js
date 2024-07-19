bf864811d10e21630795f38c9377584fe2fabd55
Evandro Florscuk <evandro.florscuk@helpertecnologia.com.br>
2024-07-19 15:50:25 -0300

    Atualizar código de jest config para aceitar o novo jest setup

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/tests'],
+    setupFiles: ['<rootDir>/jest.setup.js'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     }
