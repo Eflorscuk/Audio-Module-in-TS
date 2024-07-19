1ad419a77261cdf13bcb603a314ecb6b39dec123
Evandro Florscuk <evandro.florscuk@helpertecnologia.com.br>
2024-07-19 15:51:42 -0300

    Adicionar nova rota de áudio e criar camada de api play sound

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import routes from './routes/routes';
+import home from './routes/route';
+import audioRoutes from './routes/audioRoutes';
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 
-app.use('/', routes);
-app.use('/play_sound', routes);
+app.use('/', home);
+app.use('/api', audioRoutes);
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
 
